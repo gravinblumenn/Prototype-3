78c56d2724dff80d5947cfc388149cf90879c5a4
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-22 13:53:53 -0600

    Initial Prototype 3 Commit
    Initial Prototype 3 Commit

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerControll playerControllScript;
+    private float leftBound = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllScript = GameObject.Find("Player").GetComponent<PlayerControll>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerControllScript.gameOver == false)
+        {
+        transform.Translate (Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerControll playerControllScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
+        playerControllScript = GameObject.Find("Player").GetComponent<PlayerControll>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObstacle()
+    {
+        if(playerControllScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
